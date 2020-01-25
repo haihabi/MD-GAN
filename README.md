4fca531f664e4052fc63bbd07d2addf25852bb66
haihabi1986@gmail.com <K18118Wre>
2020-01-25 19:06:16 +0200

    update README.md, md_gan_training.py and training notebook

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # MD-GAN
 
-This repository contains PyTorch implementation of [MD-GAN](http://openaccess.thecvf.com/content_CVPR_2019/papers/Eghbal-zadeh_Mixture_Density_Generative_Adversarial_Networks_CVPR_2019_paper.pdf), along with notebook for training of Mixture of Gaussion. This repository is highly base on the following repositoy: https://github.com/eghbalz/mdgan
+This repository contains PyTorch implementation of [MD-GAN](http://openaccess.thecvf.com/content_CVPR_2019/papers/Eghbal-zadeh_Mixture_Density_Generative_Adversarial_Networks_CVPR_2019_paper.pdf), along with notebook for training. 
+This repository is highly base on the following repositoy: https://github.com/eghbalz/mdgan
 
+The repository contains a [training notebook](examples/training_md_gan.ipynb) for a Mixture of Gaussion.
 # Contributing
 If you find a bug, create a GitHub issue. Similarly, if you have questions, simply post them as GitHub issues.
--- md_gan_training.py
+++ md_gan_training.py
@@ -5,17 +5,6 @@ from losses import gaussian_likelihood_sum
 import torch.autograd as autograd
 
 
-#
-# def enable_gradients(input_module):
-#     for p in input_module.parameters():  # reset requires_grad
-#         p.requires_grad = True  # they are set to False below in netG update
-#
-#
-# def disable_gradients(input_module):
-#     for p in input_module.parameters():  # reset requires_grad
-#         p.requires_grad = False  # they are set to False below in netG update
-
-
 class MDGANTraining(object):
     def __init__(self, input_net_d: nn.Module, input_net_g: nn.Module, input_optimizer_d: Optimizer,
                  input_optimizer_g: Optimizer,
@@ -82,64 +71,7 @@ class MDGANTraining(object):
 
         fake = self.net_g(noise)
 
-        # e_generator = self.net_d(fake)
-        # lk_fake = gaussian_likelihood_sum(e_generator, self.simplex)
         g_loss = torch.log(self.epsilon + self.lambda_shared - self.calculate_likelihood(fake)).mean()
         g_loss.backward()
         self.optimizer_g.step()
         return g_loss.item()
-
-# def train_discriminator(input_net_d: nn.Module, input_net_g: nn.Module, input_optimizer_d, real_data: torch.Tensor,
-#                         input_batch_size: int, z_size: int,
-#                         input_simplex,
-#                         epsilon: float,
-#                         lambda_shared: float,
-#                         input_working_device: torch.device):
-#     # for p in input_net_d.parameters():  # reset requires_grad
-#     #     p.requires_grad = True  # they are set to False below in netG update
-#     enable_gradients(input_net_d)
-#     # real_data_v = real_data
-#     input_net_d.zero_grad()
-#     ######################################################################
-#     # train with real
-#     ######################################################################
-#     e_real = input_net_d(real_data)
-#     lk_real = gaussian_likelihood_sum(e_real, input_simplex)
-#     d_real_loss = -torch.log(epsilon + lk_real).mean()
-#     d_real_loss.backward()
-#     ######################################################################
-#     # train with fake
-#     ######################################################################
-#     noise = torch.randn(input_batch_size, z_size).to(input_working_device)
-#     with torch.no_grad():
-#         noisev = autograd.Variable(noise).to(input_working_device)  # totally freeze netG
-#     fake = autograd.Variable(input_net_g(noisev).data).to(input_working_device)
-#
-#     e_fake = input_net_d(fake)
-#     lk_fake = gaussian_likelihood_sum(e_fake, input_simplex)
-#     d_fake_loss = -torch.log(epsilon + lambda_shared - lk_fake).mean()
-#     d_fake_loss.backward()
-#     ######################################################################
-#     # calculate loss function and update weights
-#     ######################################################################
-#     d_cost = (d_fake_loss + d_real_loss).item()
-#     input_optimizer_d.step()
-#     return d_cost
-#
-#
-# def train_generator(input_net_d, input_net_g, input_optimizer_g, input_batch_size, z_size, input_simplex,
-#                     epsilon: float,
-#                     lambda_shared: float,
-#                     input_working_device):
-#     disable_gradients(input_net_d)
-#
-#     input_net_g.zero_grad()
-#     noise = autograd.Variable(torch.randn(input_batch_size, z_size)).to(input_working_device)
-#     fake = input_net_g(noise)
-#
-#     e_generator = input_net_d(fake)
-#     lk_fake = gaussian_likelihood_sum(e_generator, input_simplex)
-#     g_loss = torch.log(epsilon + lambda_shared - lk_fake).mean()
-#     g_loss.backward()
-#     input_optimizer_g.step()
-#     return g_loss.item()
